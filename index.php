f1065951890bef1e8f2a0480264dcccf6ed6cfc8
Raed <raed@riseup.net>
2015-12-26 22:48:35 +0100

    Updaated demo

--- index.php
+++ index.php
@@ -12,74 +12,31 @@
                 font-size: 2em;
             }
         </style>
-        <h1>Iconify</h1><p>Turn ugly hashes into beautiful icons</p><br>
+        <h1>Iconify</h1><p>Turn ugly hashes into beautiful icons</p
         <p>Pull requests are welcome.</p>
         <hr>
-
         <h2>TEST : </h2>
         You can test with these hashes: <br>
-        df226c2c6dcb1d995c0299a33a084b201544293c31fc3d279530121d36bbcea9 <br>
-        d89c92b4400b15c39e462a8caa939ab40c3aeeea:1234 <br>
         $racf$*USER*FC2577C6EBE6265B<br>
-
-
+        df226c2c6dcb1d995c0299a33a084b201544293c31fc3d279530121d36bbcea9<br>
+        d89c92b4400b15c39e462a8caa939ab40c3aeeea:1234<br>
 
         <form action="index.php" method="get">
             <input type="text" name="msg">
             <button type="submit">Submit</button>
         </form>
-        <span> iconHash : </span>
         <?php
         if (isset($_GET['msg']) && $_GET['msg'] != "") {
             $msg = $_GET['msg'];
-            //$broken = str_split($msg);
 
-            $broken = sha1($msg);
-            $broken = str_split($broken);
+            require './src/Iconify.php';
+            $iconify = new Iconify\Iconify();
             
-            $i = 0;
-            $len = 0;
-            if (count($broken) % 2 == 1) {
-                $len = 1;
-            }
-            $icon = "";
-            $color = "";
-            $col = 0;
+            $buffer = $iconify->doIt($msg);
 
-
-            for ($index = 0; $index < count($broken); $index++) {
-
-                if (is_numeric($broken[$index])) {
-                    $color = colorify($broken[$index]);
-                    $col++;
-                    if ($col == 2) {
-                        $icon = "glyphicon-stop";
-                        $col = 0;
-                    }
-
-                    if ($index == count($broken) - 1) {
-                        $icon = "glyphicon-stop";
-                    }
-                } else {
-
-                    if (preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $broken[$index])) {
-                        echo ("<span class='special'>" . $broken[$index] . "</span>");
-                    }
-
-                    $icon = iconify($broken[$index]);
-                }
-
-                if (isset($icon) && $icon != NULL) {
-                    if ($color == NULL) {
-                        $color = "black";
-                    }
-                }
-
-                if ($icon != NULL && $color != NULL) {
-                    echo '<span style="color:' . $color . '" class="glyphicon ' . $icon . '"></span>';
-                    $color = NULL;
-                    $icon = NULL;
-                }
+            echo "<span>IconHash: </span>";
+            foreach ($buffer as $icon) {
+                echo '<span style="color:' . $icon["color"] . '" class="glyphicon ' . $icon["icon"] . '"></span>';
             }
         }
         ?>
@@ -102,111 +59,5 @@
             ga('create', 'UA-52412775-1', 'auto');
             ga('send', 'pageview');
         </script>
-
-
     </body>
 </html>
-
-<?php
-
-function iconify($char) {
-
-    if (strlen($char) != 1) {
-        return -1;
-    }
-    if (!ctype_alpha($char)) {
-        return -1;
-    }
-    $upper = strtoupper($char);
-
-    switch ($upper) {
-        case 'A':
-            return "glyphicon-cloud";
-        case 'B':
-            return "glyphicon-envelope";
-        case 'C':
-            return "glyphicon-pencil";
-        case 'D':
-            return "glyphicon-heart";
-        case 'E':
-            return "glyphicon-star";
-        case 'F':
-            return "glyphicon-trash";
-        case 'G':
-            return "glyphicon-home";
-        case 'H':
-            return "glyphicon-road";
-        case 'I':
-            return "glyphicon-lock";
-        case 'J':
-            return "glyphicon-flag";
-        case 'K':
-            return "glyphicon-headphones";
-        case 'L':
-            return "glyphicon-camera";
-        case 'M':
-            return "glyphicon-gift";
-        case 'N':
-            return "glyphicon-fire";
-        case 'O':
-            return "glyphicon-eye-open";
-        case 'P':
-            return "glyphicon-plane";
-        case 'Q':
-            return "glyphicon-magnet";
-        case 'R':
-            return "glyphicon-bell";
-        case 'S':
-            return "glyphicon-globe";
-        case 'T':
-            return "glyphicon-phone-alt";
-        case 'U':
-            return "glyphicon-scissors";
-        case 'V':
-            return "glyphicon-sunglasses";
-        case 'W':
-            return "glyphicon--tree-deciduous";
-        case 'X':
-            return "glyphicon-remove";
-        case 'Y':
-            return "glyphicon-wrench";
-        case 'Z':
-            return "glyphicon-user";
-    }
-}
-
-function colorify($char) {
-
-    if (strlen($char) != 1) {
-        return -1;
-    }
-    if (!is_numeric($char)) {
-        return -1;
-    }
-    $upper = strtoupper($char);
-
-    switch ($upper) {
-        case '0':
-            return "#ff0000";
-        case '1':
-            return "#ff9100";
-        case '2':
-            return "#ddff00";
-        case '3':
-            return "#4cff00";
-        case '4':
-            return "#00ffd4";
-        case '5':
-            return "#0099ff";
-        case '6':
-            return "#0008ff";
-        case '7':
-            return "#ff00e5";
-        case '8':
-            return "#ff0055";
-        case '9':
-            return "#448700";
-    }
-}
-?>
-
